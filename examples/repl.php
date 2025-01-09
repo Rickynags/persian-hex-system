07bb49ed4e7bcd353c5fac5a2959d0e8a1a1e2cf
ALI Mohammadiyeh (Max Base) <MaxBaseCode@gmail.com>
2025-01-10 00:19:06 +0100

    test

--- examples/repl.php
+++ examples/repl.php
@@ -1,21 +1,20 @@
 <?php
 require_once __DIR__ . "/../libs/persian_hex.php";
 
-// try {
-//     echo "شماره وارد کنید: ";
-//     $input = trim(fgets(STDIN));
-//     $number = (int)$input;
+try {
+    echo "شماره وارد کنید: ";
+    $input = trim(fgets(STDIN));
+    $number = (int)$input;
 
-//     $persianHex = new PersianHex($number);
-//     echo $persianHex->show() . PHP_EOL;
-// } catch (Exception $e) {
-//     echo "خطا: لطفاً یک عدد صحیح معتبر وارد کنید. " . $e->getMessage() . PHP_EOL;
-// }
-
-$res = [];
-for ($i = 0; $i <= 10000; $i++) {
-    $persianHex = new PersianHex($i);
-    // echo $persianHex->show() . PHP_EOL;
-    $res[$i] = $persianHex->show();
+    $persianHex = new PersianHex($number);
+    echo $persianHex->show() . PHP_EOL;
+} catch (Exception $e) {
+    echo "خطا: لطفاً یک عدد صحیح معتبر وارد کنید. " . $e->getMessage() . PHP_EOL;
 }
-file_put_contents('../persian_hex.txt', json_encode($res));
+
+// $res = [];
+// for ($i = 10000; $i >= 0; $i--) {
+//     $persianHex = new PersianHex($i);
+//     $res[$i . ""] = $persianHex->show();
+// }
+// file_put_contents('../persian_hex.txt', json_encode($res, JSON_UNESCAPED_UNICODE));
